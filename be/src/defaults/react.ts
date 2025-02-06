197d46f57b5356eaabb275b9a27589cbae6a8c7a
kotiyalashwin <ashwinkotiyal07@gmail.com>
2025-02-06 18:24:03 +0000

    LLM id type

--- be/src/defaults/react.ts
+++ be/src/defaults/react.ts
@@ -3,3 +3,7 @@ export const basePromp =
 
 export const refPrompt =
   "For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos.\n\nUse stock photos from unsplash where appropriate, only valid URLs you know exist. Do not download the images, only link to them in image tags.";
+
+export const runningCommand = (userprompt: string) => {
+  return `<bolt_running_commands>\n</bolt_running_commands>\n\nCurrent Message:\n\n${userprompt}\n\nFile Changes:\n\nHere is a list of all files that have been modified since the start of the conversation.\nThis information serves as the true contents of these files!\n\nThe contents include either the full file contents or a diff (when changes are smaller and localized).\n\nUse it to:\n - Understand the latest file modifications\n - Ensure your suggestions build upon the most recent version of the files\n - Make informed decisions about changes\n - Ensure suggestions are compatible with existing code\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - /home/project/.bolt/config.json`;
+};
--- be/src/index.ts
+++ be/src/index.ts
@@ -1,33 +1,81 @@
 import Anthropic from "@anthropic-ai/sdk";
+import express from "express";
 require("dotenv").config();
-import { basePrompt } from "./defaults/react";
+import {
+  basePromp,
+  basePromp as reactBasePrompt,
+  refPrompt as reactRefPrompt,
+  runningCommand as reactRunning,
+} from "./defaults/react";
+
+import {
+  basePrompt as nodeBasePrompt,
+  runningCommand as nodeRunnig,
+} from "./defaults/node";
+import { TextBlock } from "@anthropic-ai/sdk/resources";
 const anthropic = new Anthropic();
+const app = express();
+app.use(express.json());
 
-async function main() {
-  await anthropic.messages
-    .stream({
-      model: "claude-3-5-sonnet-20241022",
-      max_tokens: 8192,
-      temperature: 0,
-      messages: [
-        {
-          role: "user",
-          content:
-            "For all the apps use a generalised way and make them beautiful",
-        },
-        {
-          role: "user",
-          content: basePrompt,
-        },
-        {
-          role: "user",
-          content: "create a todo app",
-        },
-      ],
-    })
-    .on("text", (response) => {
-      console.log(response);
+app.post("/template", async (req, res) => {
+  const prompt = req.body.prompt;
+
+  const response = await anthropic.messages.create({
+    model: "claude-3-5-sonnet-20241022",
+    max_tokens: 8192,
+    temperature: 0,
+    messages: [
+      {
+        role: "user",
+        content: `For the given message identify wether it is a react appliation or nodejs application. Once you identify, your response should be either "react" or "nodejs". Do not respond with any other value than any one of these texts. The prompt is : ${prompt}. `,
+      },
+    ],
+  });
+
+  const answer = (response.content[0] as TextBlock).text;
+
+  if (answer === "react") {
+    res.json({
+      prompts: [reactBasePrompt, reactRefPrompt, reactRunning(prompt)],
     });
-}
+    return;
+  }
+  if (answer === "nodejs") {
+    res.json({
+      prompts: [nodeBasePrompt, nodeRunnig(prompt)],
+    });
 
-main();
+    return;
+  }
+});
+
+// async function main() {
+//   await anthropic.messages
+//     .stream({
+//       model: "claude-3-5-sonnet-20241022",
+//       max_tokens: 8192,
+//       temperature: 0,
+//       messages: [
+//         {
+//           role: "user",
+//           content:
+//             "For all the apps use a generalised way and make them beautiful",
+//         },
+//         {
+//           role: "user",
+//           content: "fafaf",
+//         },
+//         {
+//           role: "user",
+//           content: "create a todo app",
+//         },
+//       ],
+//     })
+//     .on("text", (response) => {
+//       console.log(response);
+//     });
+// }
+
+// main();
+
+app.listen(8080);
