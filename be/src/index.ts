2b2a653da51e015eeb9d33cdc53c027102fab191
kotiyalashwin <ashwinkotiyal07@gmail.com>
2025-02-07 11:08:51 +0000

    template endpoint done

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,18 +1,10 @@
 import Anthropic from "@anthropic-ai/sdk";
 import express from "express";
 require("dotenv").config();
-import {
-  basePromp,
-  basePromp as reactBasePrompt,
-  refPrompt as reactRefPrompt,
-  runningCommand as reactRunning,
-} from "./defaults/react";
-
-import {
-  basePrompt as nodeBasePrompt,
-  runningCommand as nodeRunnig,
-} from "./defaults/node";
+import { basePrompt as reactBasePrompt } from "./defaults/react";
+import { basePrompt as nodeBasePrompt } from "./defaults/node";
 import { TextBlock } from "@anthropic-ai/sdk/resources";
+import { BASE_PROMPT } from "./prompt";
 const anthropic = new Anthropic();
 const app = express();
 app.use(express.json());
@@ -36,19 +28,35 @@ app.post("/template", async (req, res) => {
 
   if (answer === "react") {
     res.json({
-      prompts: [reactBasePrompt, reactRefPrompt, reactRunning(prompt)],
+      prompts: [
+        BASE_PROMPT,
+        `Here is an artifact that contains all the availbale files to you.\n Consider the content of ALL the files in the project.\n\n${reactBasePrompt}\n\n Here is a list of files that are hidden from you but exist on the system and are not shown to you:\n\n -.gitignore\n -package-lock.json\n`,
+      ],
+      //seperate the ui prompt
+      uiPrompts: [reactBasePrompt],
     });
     return;
   }
   if (answer === "nodejs") {
     res.json({
-      prompts: [nodeBasePrompt, nodeRunnig(prompt)],
+      prompts: [
+        `Here is an artifact that contains all the availbale files to you.\n Consider the content of ALL the files in the project.\n\n${nodeBasePrompt}\n\n Here is a list of files that are hidden from you but exist on the system and are not shown to you:\n\n -.gitignore\n -package-lock.json\n`,
+      ],
+      //seperate the ui prompt
+      uiPrompt: [nodeBasePrompt],
     });
 
     return;
   }
 });
 
+//TO CLAUDE
+//1st message - list of all project files visible to you
+//2nd message(only React) - for all i tell make them beautifule
+//3rd message - bolt running command. will contain what user has asked for
+
+//Chat endpoint
+//simply the prompt from the user
 // async function main() {
 //   await anthropic.messages
 //     .stream({
